6e95b04f7f8e3eb3f7be4c3b7287f5b7d3628cd5
ConcerroX <concerrox@outlook.com>
2025-08-01 22:56:00 +0800

    Remove unused editor

--- src/editor/global.d.ts
+++ src/editor/global.d.ts
@@ -1,10 +1,10 @@
-declare module "monaco-editor/esm/vs/language/typescript/tsMode" {
-  export class SuggestAdapter {
-    provideCompletionItems(
-      model: import("monaco-editor").editor.ITextModel,
-      position: import("monaco-editor").Position,
-      _context: import("monaco-editor").languages.CompletionContext,
-      token: import("monaco-editor").CancellationToken,
-    ): Promise<import("monaco-editor").languages.CompletionList | undefined>;
-  }
-}
+// declare module "monaco-editor/esm/vs/language/typescript/tsMode" {
+//   export class SuggestAdapter {
+//     provideCompletionItems(
+//       model: import("monaco-editor").editor.ITextModel,
+//       position: import("monaco-editor").Position,
+//       _context: import("monaco-editor").languages.CompletionContext,
+//       token: import("monaco-editor").CancellationToken,
+//     ): Promise<import("monaco-editor").languages.CompletionList | undefined>;
+//   }
+// }
